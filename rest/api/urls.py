8ed7b175de32e77aec2c3294c9ac083b85b689f3
vishalupadhyay7389 <120776857+vishalupadhyay7389@users.noreply.github.com>
2024-03-08 18:33:59 +0530

    first commit

---
+++ rest/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.SignUp , name='signup'),
+    path('login/', views.LoginPage , name='login'),
+    path('home/', views.HomePage , name='home'),
+    path('logout/', views.LogoutPage , name = 'logout'),
+    
+]
